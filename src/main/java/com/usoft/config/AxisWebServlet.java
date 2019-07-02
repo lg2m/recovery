9d51f862b1e35e9b2d61e40c644ec5a6bf841685
ikaven1024 <ikaven1024@gmail.com>
2019-07-03 01:05:59 +0800

    init

--- src/main/java/com/usoft/config/AxisWebServlet.java
+++ src/main/java/com/usoft/config/AxisWebServlet.java
@@ -1,13 +1,11 @@
 package com.usoft.config;
 
-
 import org.apache.axis.transport.http.AxisServlet;
-import org.springframework.boot.autoconfigure.web.ServerProperties;
-import org.springframework.boot.autoconfigure.web.ServerProperties.Servlet;
 
-//import javax.servlet.annotation.WebServlet;
-//import javax.servlet.http.HttpServlet;
+import javax.servlet.Servlet;
+import javax.servlet.annotation.WebServlet;
 
-//@WebServlet(urlPatterns = "/services/*", loadOnStartup = 1)
-//public abstract class AxisWebServlet extends AxisServlet implements Servlet {
-//}
+@WebServlet(urlPatterns = "/services/*", loadOnStartup = 1)
+public class AxisWebServlet extends AxisServlet implements Servlet{
+
+}
